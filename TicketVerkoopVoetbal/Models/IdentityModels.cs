3f325700fb02eaab35e41eaa739b287c12fa6c2b
blackterror25 <benborra@hotmail.com>
2016-04-26 15:03:40 +0200

    Connection met nieuwe database

--- TicketVerkoopVoetbal/Models/IdentityModels.cs
+++ TicketVerkoopVoetbal/Models/IdentityModels.cs
@@ -9,7 +9,8 @@ namespace TicketVerkoopVoetbal.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Username { get; internal set; }
+        public string FirstName { get; set; }
+        public string Name { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
