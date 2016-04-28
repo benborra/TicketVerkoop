7f9faa5662682d1ff69e5c6825704ae8c94d68ea
Arno Dhondt <arno.dhondt@gmail.com>
2016-04-28 11:07:25 +0200

    Stadions opvragen lukt, naar edit gaan ook. Nog te doen: koppelen save button aan update query (via Actionresult Edit() Toegevoegd: Ticket.Service

---
+++ Ticket.Service/StadionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.DAO;
+using Ticket.Model;
+
+namespace Ticket.Service
+{
+   public class StadionService
+    {
+        private  StadionDAO stadionDao;
+
+        public StadionService()
+        {
+            stadionDao = new StadionDAO();
+        }
+
+        public IEnumerable<Stadion> All()
+        {
+            return stadionDao.All();
+        }
+
+        public Stadion Get(int id)
+        {
+            return stadionDao.Get(id);
+        }
+    }
+}
---
+++ TicketVerkoopVoetbal/Controllers/StadionController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ticket.Model;
+using Ticket.Service;
+
+namespace TicketVerkoopVoetbal.Controllers
+{
+    public class StadionController : Controller
+    {
+
+        private StadionService stadionService;
+        // GET: Stadion
+        public ActionResult Index()
+        {
+            stadionService = new StadionService();
+            var stadions = stadionService.All();
+            if (stadions == null)
+            {
+                return HttpNotFound();
+            }
+            // in Beers staat dit zo , maar in de view wordt er gelijk niets mee gedaan
+            // wat is het verschil tussen model.Naam en de viewbag?
+            ViewBag.stadions =
+                   new SelectList(stadionService.All(), "naam", "adres");
+            return View(stadions);
+
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            stadionService = new StadionService();
+            Stadion stadion = stadionService.Get(Convert.ToInt32(id));
+            if (stadion == null)
+            {
+                return HttpNotFound();
+            }
+
+            
+            ViewBag.StadionNr =
+                    new SelectList(stadionService.All(), "Brouwernr", "Naam");
+
+            return View(stadion);
+        }
+
+        public ActionResult Save()
+        {
+            // als er op de button save wordt geklikt komen we hiernaartoe
+            // insert update statement met controle
+            // terugkeren naar de lijst, die zal geupdate zijn.
+            return RedirectToAction("Index");
+        }
+    }
+}
