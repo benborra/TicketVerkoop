8ade53923b087f58ef08a172985a7c291f00be64
Arno Dhondt <boens93@hotmail.com>
2016-05-01 14:19:36 +0200

    ing

--- Ticket.DAO/WedstrijdDAO.cs
+++ Ticket.DAO/WedstrijdDAO.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Ticket.Model;
-using System.Data.Entity;
 
 namespace Ticket.DAO
 {
-    public class ClubDAO
+    class WedstrijdDAO
     {
-        public IEnumerable<Clubs> All()
+        public IEnumerable<Wedstrijd> All()
         {
             using (var db = new TicketEntities())
             {
-                return db.Clubs.ToList();
+                return db.Wedstrijd.ToList();
             }
         }
 
-        public Clubs Get(int id)
+
+        public Wedstrijd Get(int id)
         {
             using (var db = new TicketEntities())
             {
-                return db.Clubs.Where(c => c.id == id).First();
+                return db.Wedstrijd.Where(c => c.id == id).First();
             }
         }
 
-        // methods to write:
-        // public 
+        // get wedstrijdPerPloeg
+        // get wedstrijd per datum
+         
     }
 }
