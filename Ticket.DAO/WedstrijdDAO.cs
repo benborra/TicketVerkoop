9f355cdacef28589e2d475874e16bcd1f4004eff
blackterror25 <Benborra@hotmail.com>
2016-05-02 15:09:55 +0200

    Removed Ignore files from cache

---
+++ Ticket.DAO/WedstrijdDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Model;
+
+namespace Ticket.DAO
+{
+    class WedstrijdDAO
+    {
+        public IEnumerable<Wedstrijd> All()
+        {
+            using (var db = new TicketEntities())
+            {
+                return db.Wedstrijd.ToList();
+            }
+        }
+
+
+        public Wedstrijd Get(int id)
+        {
+            using (var db = new TicketEntities())
+            {
+                return db.Wedstrijd.Where(c => c.id == id).First();
+            }
+        }
+
+        // get wedstrijdPerPloeg
+
+        public IEnumerable<Wedstrijd> GetWedStrijdPerPloeg(int id)
+        {
+            using (var db = new TicketEntities())
+            {
+                return db.Wedstrijd.Where(r => r.thuisPloeg == id ||  r.bezoekersPloeg == id).ToList();
+            }
+
+            
+        }
+        // get wedstrijd per datum
+
+        public IEnumerable<Wedstrijd> WedstrijdPerDatum(DateTime time)
+        {
+            using (var db = new TicketEntities())
+            {
+                // geen datetime om op te filteren!
+                // return db.Wedstrijd.Where(r => r.).ToList();
+            }
+            return null;
+        }
+         
+    }
+}
---
+++ Ticket.Service/TicketService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Model;
+using Ticket.DAO;
+
+namespace Ticket.Service
+{
+    public class TicketService
+    {
+        TicketDAO ticketDao;
+
+        public TicketService()
+        {
+            ticketDao = new TicketDAO();
+        }
+        public IEnumerable<Tickets> All()
+        {
+            return ticketDao.All();
+        }
+
+       
+        public Tickets GetById(int id)
+        {
+            return ticketDao.GetById(id);
+        }
+        // TE TESTEN
+        public IEnumerable<Tickets> getTicketsperPersoon(int persoonId)
+        {
+            return ticketDao.getTicketsperPersoon(persoonId);
+        }
+        // TE TESTEN
+        public IEnumerable<Tickets> GetTicketsPerPersoonPerWedstrijd(int persId, Wedstrijd wedstrijd)
+        {
+            return ticketDao.GetTicketsPerPersoonPerWedstrijd(persId, wedstrijd);
+        }
+
+        public IEnumerable<Tickets> getTicketsPerWedstrijd(Wedstrijd wedstrijd)
+        {
+            return ticketDao.getTicketsPerWedstrijd(wedstrijd);
+        }
+    }
+}
